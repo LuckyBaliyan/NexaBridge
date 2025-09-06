64e01b9040a5510209da767a044f3e628fc09eb1
Lucky Baliyan <baliyanlucky85@gmail.com>
2025-09-06 19:56:44 +0530

    fix for tablets

--- src/components/ui/sections/Hero.jsx
+++ src/components/ui/sections/Hero.jsx
@@ -8,13 +8,13 @@ const Hero = () => {
   return (
     <div className='relative w-full h-screen hero'>
         <div className="date-wrapper flex justify-between py-25">
-          <div className="heading-wrapper pr-0 items-center md:p-4 md:items-start flex flex-col gap-12 md:gap-6">
+          <div className="heading-wrapper pr-0 items-center lg:p-4 lg:items-start flex flex-col gap-12 md:gap-20 lg:gap-6">
             <h1 className='leading-[0.9] text-start tracking-tighter'>Welcome To <br /> Nexa Bridge</h1>
-            <p className='text-sm px-2  text-center md:text-start md:text-base w-[100%] md:w-[50%] md:pl-2 leading-[1]'>Connect with your alumni network like never before. 
+            <p className='text-sm px-2  text-center lg:text-start md:text-base w-[100%] lg:w-[50%] lg:pl-2 leading-[1]'>Connect with your alumni network like never before. 
               NexaBridge offers a comprehensive platform to foster engagement, 
               mentorship, and career opportunities for your institution's graduates.
             </p>
-            <div className="img-wrapper w-[36vh] h-[24vh] self-center  md:hidden block">
+            <div className="img-wrapper w-[36vh] h-[24vh] self-center  lg:hidden block">
             <img src="/images/hero.png" alt=""/>
             </div>
            <div className='flex gap-2 px-0'>
@@ -26,7 +26,7 @@ const Hero = () => {
             text-gray-700 py-3 px-6 rounded-md"/>
            </div>
           </div>
-          <div className="img-wrapper hidden md:relative md:block pr-20">
+          <div className="img-wrapper hidden md:relative lg:block pr-20">
             <img src="/images/hero.png" alt=""/>
           </div>
         </div>
